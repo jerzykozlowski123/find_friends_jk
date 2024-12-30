cb4ccc8f804518acfbccdd64fc2e7f45c3a7bced
Jerzy Kozlowski <jerzykozlowski123@gmail.com>
2024-12-30 20:00:57 +0100

    korekta selectboxów

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def get_all_participants():
 with st.sidebar:
     st.header("Powiedz nam coś o sobie")
     st.markdown("Pomożemy Ci znaleźć osoby, które mają podobne zainteresowania")
-    age = st.selectbox("Wiek", ["Wybierz", '<18', '18-24', '25-34', '35-44', '45-54', '55-64', '>=65', 'unknown'])
-    edu_level = st.selectbox("Wykształcenie", ["Wybierz", 'Podstawowe', 'Średnie', 'Wyższe'])
-    fav_animals = st.selectbox("Ulubione zwierzęta", ["Wybierz", 'Brak ulubionych', 'Psy', 'Koty', 'Koty i Psy', 'Inne'])
-    fav_place = st.selectbox("Ulubione miejsce", ["Wybierz", 'Nad wodą', 'W lesie', 'W górach', 'Inne'])
+    age = st.selectbox("Wiek", ['<18', '18-24', '25-34', '35-44', '45-54', '55-64', '>=65', 'unknown'])
+    edu_level = st.selectbox("Wykształcenie", ['Podstawowe', 'Średnie', 'Wyższe'])
+    fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Psy', 'Koty', 'Koty i Psy', 'Inne'])
+    fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
     gender = st.radio("Płeć", ['Mężczyzna', 'Kobieta'])
 
     person_df = pd.DataFrame([
